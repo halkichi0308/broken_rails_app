0aec71af17b9499f983aed5d516c3e3449b38f9b
halkichi0308 <m.kouda0308@gmail.com>
2023-02-12 17:48:46 +0900

    Add https setting

--- init/init.sh
+++ init/init.sh
@@ -13,4 +13,6 @@ if [ -e tmp/pids/server.pid ]; then echo 0 > tmp/pids/server.pid; fi
 rails db:migrate && \
 rails db:seed
 
+#rails s -b 'ssl://0.0.0.0:3000?key=/usr/src/broken_rails_app/config/localhost.key&cert=/usr/src/broken_rails_app/config/localhost.crt'
+
 rails s -b 0.0.0.0
